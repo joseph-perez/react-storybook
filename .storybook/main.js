e889974a32d633c143bca31bda4bd15cf2fee2b2
Joseph Perez <josephperez717@gmail.com>
2025-03-31 19:12:58 -0600

    start to setup chromatic

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,12 @@ module.exports = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 
 	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-mdx-gfm',
-		'@storybook/addon-webpack5-compiler-babel',
-	],
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-mdx-gfm',
+        '@storybook/addon-webpack5-compiler-babel',
+        "@chromatic-com/storybook"
+    ],
 
 	framework: {
 		name: '@storybook/react-webpack5',
